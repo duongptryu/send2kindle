6836331601d5cd31b592d4a12266b80b5b25d7e3
duongptryu <duongptryu@gmail.com>
2021-02-27 14:10:51 +0700

    change path

--- functions/convert.py
+++ functions/convert.py
@@ -12,7 +12,7 @@ def convert_to_mobi(file_name, viber, viber_request):
     try:
         viber.send_messages(viber_request.sender.id, TextMessage(text="Converting" ))
         # os.system('ebook-convert ' + '"' + os.getcwd() + '\\download\\' + name_book +'.epub" ' + '"' + os.getcwd() + '\\download\\' + name_book + '.mobi"')
-        subprocess.call(["ebook-convert",os.getcwd() + '\\download\\' + name_book + "." + file_name[-1], os.getcwd() + '\\download\\' + name_book +'.mobi']) 
+        subprocess.call(["ebook-convert",os.getcwd() + '/download/' + name_book + "." + file_name[-1], os.getcwd() + '/download/' + name_book +'.mobi']) 
         viber.send_messages(viber_request.sender.id, TextMessage(text="Converted" ))
         delete(name_book + ".epub")
         return name_book + ".mobi"
